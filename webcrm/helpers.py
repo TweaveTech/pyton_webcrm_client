d54f3ceac20b83638dfd571eddf82ac78d72364e
Sascha Dobbelaere <sd@tweave.tech>
2024-03-20 10:53:48 +0000

    old format for compatibility

--- webcrm/helpers.py
+++ webcrm/helpers.py
@@ -22,7 +22,7 @@ def convert_dict_to_namedtuple(base_name, d):
     for k, v in deepcopy(d).items():
         if isinstance(v, dict):
             v = convert_dict_to_namedtuple(camelcase(k), v)
-        elif isinstance(v, list | set | tuple):
+        elif isinstance(v, list) or isinstance(v, set) or isinstance(v, tuple):
             v = [convert_dict_to_namedtuple(camelcase(k),i) for i in v]
         
         return_d[snake_case(k)] = v
